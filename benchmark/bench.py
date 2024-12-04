383b0fb8af2abf68b77404c1a1d1bd27ab15f807
unknown <demid.shumakher@yandex.ru>
2024-12-04 09:08:17 +0300

    added Readme

--- benchmark/bench.py
+++ benchmark/bench.py
@@ -2,7 +2,7 @@ import subprocess
 import datetime
 
 
-n = 100
+n = 1000
 print(n)
 
 
@@ -11,21 +11,21 @@ for i in range(n):
     subprocess.run(["yaml-to-json-go", "convert", "input.yaml", "output.json"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 time = datetime.datetime.now() - now
-print("util:", time )
+print("converter:", time )
 
 
 now = datetime.datetime.now()
 for i in range(n):
     subprocess.run(["./myConverter.exe", "input.yaml", "output.json"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 time = datetime.datetime.now() - now
-print("my:", time)
+print("my convert:", time)
 
 
 now = datetime.datetime.now()
 for i in range(n):
     subprocess.run(["python", "reg.py", "input.yaml", "output.json"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 time = datetime.datetime.now() - now
-print("regex:", time)
+print("regex python:", time)
 
 
 now = datetime.datetime.now()
@@ -33,4 +33,4 @@ for i in range(n):
     subprocess.run(["./regexGo.exe", "input.yaml", "output.json"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 time = datetime.datetime.now() - now
-print("regexGo:", time)
+print("regex go:", time)
